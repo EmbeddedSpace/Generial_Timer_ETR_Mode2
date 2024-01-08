c9f8cc6b280b859bbb41fae530d712f423608454
guoliang <guoliang.wang@cust.edu.cn>
2024-01-08 15:29:55 +0800

    update

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -99,7 +99,7 @@ int main(void)
   MX_TIM2_Init();
   MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
-    __HAL_TIM_CLEAR_IT(&htim2,TIM_IT_UPDATE);
+  __HAL_TIM_CLEAR_IT(&htim2,TIM_IT_UPDATE);
   HAL_TIM_Base_Start_IT(&htim2);
   /* USER CODE END 2 */
 
@@ -172,11 +172,11 @@ static void MX_TIM2_Init(void)
 
   /* USER CODE END TIM2_Init 1 */
   htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 1;
+  htim2.Init.Prescaler = 0;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim2.Init.Period = 10-1;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
   if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
   {
     Error_Handler();
